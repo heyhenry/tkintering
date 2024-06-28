815665b5b35bf2725d479fd1a9c780fb22de4fc5
heyhenry <henryyy3@hotmail.com>
2024-06-29 07:40:45 +0800

    creating initial tabbing and tkinter setup.

---
+++ reading_logger/main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+# initial tkinter setup
+root = tk.Tk()
+root.title('Reading Logger')
+root.geometry('300x300')
+
+# tabbing system initiate
+notebook = ttk.Notebook(root)
+notebook.pack(expand=True, fill='both')
+
+# tabbing outline
+home_frame = tk.Frame(notebook, bg='lightblue')
+add_entry_frame = tk.Frame(notebook, bg='lightyellow')
+
+home_frame.pack()
+add_entry_frame.pack()
+
+notebook.add(home_frame, text='Home')
+notebook.add(add_entry_frame, text='Add Entry')
+
+
+root.mainloop()
