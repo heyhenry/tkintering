a99a2f0904e83f23bdf13d15aa77248bf6b0ec5c
heyhenry <henryyy3@hotmail.com>
2024-06-29 08:19:35 +0800

    centering home title.

--- reading_logger/main.py
+++ reading_logger/main.py
@@ -1,3 +1,7 @@
+# Resources used: 
+# creating listboxes: https://www.geeksforgeeks.org/python-tkinter-listbox-widget/
+# centering home title: https://stackoverflow.com/questions/70974175/with-tkinter-grid-method-i-want-to-center-the-labels 
+
 import tkinter as tk
 from tkinter import ttk
 
@@ -9,11 +13,14 @@ root.geometry('300x300')
 # home
 home_frame = tk.Frame(root, bg='lightblue')
 home_frame.pack(expand=True, fill='both')
+home_frame.rowconfigure(0, weight=1)
+home_frame.columnconfigure(0, weight=1)
 
-home_title = tk.Label(home_frame, text='Reading Logger | Home')
-home_title.pack()
+home_title = tk.Label(home_frame, text='Reading Logger | Home', bg='lightblue')
+home_title.grid(row=0, column=0, sticky='n')
 
-
+# entries_lbl = tk.Label(home_frame, text='Entries', bg='lightblue')
+# entries_lbl.grid(row=1, column=0)
 
 
 
