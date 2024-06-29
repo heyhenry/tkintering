d00b710da9892ee7ae474eb29d1c1c00d9483b94
heyhenry <henryyy3@hotmail.com>
2024-06-29 08:28:06 +0800

    added listbox, deleting centering code.

--- reading_logger/main.py
+++ reading_logger/main.py
@@ -13,15 +13,15 @@ root.geometry('300x300')
 # home
 home_frame = tk.Frame(root, bg='lightblue')
 home_frame.pack(expand=True, fill='both')
-home_frame.rowconfigure(0, weight=1)
-home_frame.columnconfigure(0, weight=1)
 
 home_title = tk.Label(home_frame, text='Reading Logger | Home', bg='lightblue')
-home_title.grid(row=0, column=0, sticky='n')
+home_title.grid(row=0, column=0)
 
-# entries_lbl = tk.Label(home_frame, text='Entries', bg='lightblue')
-# entries_lbl.grid(row=1, column=0)
+entries_lbl = tk.Label(home_frame, text='Entries', bg='lightblue')
+entries_lbl.grid(row=1, column=0)
 
+entries_lb = tk.Listbox(home_frame)
+entries_lb.grid(row=2, column=0)
 
 
 root.mainloop()
