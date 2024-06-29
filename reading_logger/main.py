dc7b54a4574295abc7545080a6eef20e48a96d70
heyhenry <henryyy3@hotmail.com>
2024-06-29 08:49:27 +0800

    form setup created for new entry

--- reading_logger/main.py
+++ reading_logger/main.py
@@ -9,8 +9,34 @@ def new_entry_popup():
     new_entry = tk.Toplevel(home_frame, bg='lightyellow')
     new_entry.title('New Entry')
     new_entry.geometry('300x300')
+    
     new_entry_title = tk.Label(new_entry, text='Reading Logger | New Entry', bg='lightyellow')
-    new_entry_title.pack()
+    new_entry_title.grid(row=0, columnspan=2)
+
+    ne_title = tk.Label(new_entry, text='Title:', bg='lightyellow')
+    ne_type = tk.Label(new_entry, text='Type:', bg='lightyellow')
+    ne_chapread = tk.Label(new_entry, text='Chapters Read:', bg='lightyellow')
+    ne_readstat = tk.Label(new_entry, text='Reading Status:', bg='lightyellow')
+
+    ne_title_entry = tk.Entry(new_entry)
+    ne_type_entry = tk.Entry(new_entry)
+    ne_chapread_entry = tk.Entry(new_entry)
+    ne_readstat_entry = tk.Entry(new_entry)
+
+    submit_entry = tk.Button(new_entry, text='Add Entry')
+
+    ne_title.grid(row=1, column=0)
+    ne_type.grid(row=2, column=0)
+    ne_chapread.grid(row=3, column=0)
+    ne_readstat.grid(row=4, column=0)
+
+    ne_title_entry.grid(row=1, column=1)
+    ne_type_entry.grid(row=2, column=1)
+    ne_chapread_entry.grid(row=3, column=1)
+    ne_readstat_entry.grid(row=4, column=1)
+
+    submit_entry.grid(row=5, columnspan=2)
+
 
 # initial tkinter setup
 root = tk.Tk()
