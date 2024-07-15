222155bc637291d4f3c3bf5fb52cd4992330f0e8
heyhenry <henryyy3@hotmail.com>
2024-07-15 18:38:00 +0800

    added comments

--- playing_around/random_two/main.py
+++ playing_around/random_two/main.py
@@ -1,13 +1,16 @@
 import requests
 import tkinter as tk
 
+# window setup
 root = tk.Tk()
 root.title('URL Shortener')
 root.geometry("250x150+800+250")
 
+# variables
 given_url = tk.StringVar()
 result_url = tk.StringVar()
 
+# function that shortens given url
 def shorten():
     base_url = 'http://tinyurl.com/api-create.php?url='
     the_url = given_url.get()
@@ -15,6 +18,7 @@ def shorten():
     short_url = response.text
     result_url.set(short_url)
 
+# ui
 title_lbl = tk.Label(root, text='Shorten This URL')
 url_entry = tk.Entry(root, textvariable=given_url)
 shortened_url_entry = tk.Entry(root, textvariable=result_url)
