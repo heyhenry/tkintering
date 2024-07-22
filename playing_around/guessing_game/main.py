bb78a715ccadbce75d956a506c8fbf2129b2321c
heyhenry <henryyy3@hotmail.com>
2024-07-22 14:59:36 +0800

    implemented reset function and disclosing and removing irrelevant content base don action.

--- playing_around/guessing_game/main.py
+++ playing_around/guessing_game/main.py
@@ -27,6 +27,7 @@ def game_results():
         verdict.set('Noone won!')    
     
     game_summary_func()
+    reset_btn.grid(row=7, columnspan=2)
 
 def game_summary_func():
     player_num = player_choice.get()
@@ -36,16 +37,38 @@ def game_summary_func():
     ai_verdict.set(f'AI selected the number {ai_choice_generation}')
     random_num_showcase.set(f'The random number generated was {randnum}')
 
-game_title = tk.Label(root, text='Numbers Guessing Game', font=('Maven Black Pro', 16))
-player_lbl = tk.Label(root, text="Player's Guess:")
-player_entry = tk.Entry(root, textvariable=player_choice)
-submit_choice = tk.Button(root, text='Run the Round!', command=game_results)
+def reset():
+    global randnum
+    global ai_choice_generation
+    randnum = random.randint(1, 10)
+    ai_choice_generation = random.randint(1, 10)
 
-verdict_lbl = tk.Label(root, textvariable=verdict)
-game_summary = tk.Label(root, textvariable=game_summary_title)
-player_summary = tk.Label(root, textvariable=player_verdict)
-ai_summary = tk.Label(root, textvariable=ai_verdict)
-randnum_summary = tk.Label(root, textvariable=random_num_showcase)
+    player_choice.set('')
+    ai_choice.set('')
+    verdict.set('')
+
+    game_summary_title.set('')
+    random_num_showcase.set('')
+    player_verdict.set('')
+    ai_verdict.set('')
+
+    submit_choice.grid(row=2, columnspan=2, pady=10)
+
+game_screen = tk.Frame(root)
+game_screen.grid(row=0, column=0)
+
+game_title = tk.Label(game_screen, text='Numbers Guessing Game', font=('Maven Black Pro', 16))
+player_lbl = tk.Label(game_screen, text="Player's Guess:")
+player_entry = tk.Entry(game_screen, textvariable=player_choice)
+submit_choice = tk.Button(game_screen, text='Run the Round!', command=game_results)
+
+verdict_lbl = tk.Label(game_screen, textvariable=verdict)
+game_summary = tk.Label(game_screen, textvariable=game_summary_title)
+player_summary = tk.Label(game_screen, textvariable=player_verdict)
+ai_summary = tk.Label(game_screen, textvariable=ai_verdict)
+randnum_summary = tk.Label(game_screen, textvariable=random_num_showcase)
+
+reset_btn = tk.Button(game_screen, text='Reset', command=reset)
 
 game_title.grid(row=0, columnspan=2, padx=10)
 player_lbl.grid(row=1, column=0)
@@ -57,6 +80,9 @@ game_summary.grid(row=3, columnspan=2)
 player_summary.grid(row=4, columnspan=2)
 ai_summary.grid(row=5, columnspan=2)
 randnum_summary.grid(row=6, columnspan=2, pady=(0, 10))
+
+reset_btn.grid(row=7, columnspan=2)
+reset_btn.grid_forget()
 
 root.mainloop()
 
