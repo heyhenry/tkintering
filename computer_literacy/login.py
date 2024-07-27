3a1a316536d0a57ecfd3ef6858c2969c7b574158
heyhenry <henryyy3@hotmail.com>
2024-07-28 00:05:33 +0800

    implemented logic to censoring and uncensor password

--- computer_literacy/login.py
+++ computer_literacy/login.py
@@ -8,12 +8,23 @@ root.geometry('300x150')
 
 username = tk.StringVar()
 password = tk.StringVar()
+uncensor_pressed = True
 
 # def validate_login():
 
 def test():
     login_frame.destroy()
     profile_frame.pack()
+
+# censoring and uncensoring password upon click
+def uncensor_password():
+    global uncensor_pressed
+    if uncensor_pressed:
+        password_entry.config(show='')
+        uncensor_pressed = False
+    else:
+        password_entry.config(show='*')
+        uncensor_pressed = True
 
 login_frame = ttk.Frame(root)
 login_frame.pack()
@@ -30,6 +41,8 @@ password = ttk.Label(login_frame, text='Password:')
 password.grid(row=2, column=0)
 password_entry = ttk.Entry(login_frame, show='*')
 password_entry.grid(row=2, column=1)
+uncensor_entry = ttk.Button(login_frame, command=uncensor_password)
+uncensor_entry.grid(row=2, column=2)
 
 login = ttk.Button(login_frame, text='Login', command=test)
 login.grid(row=3, columnspan=2)
