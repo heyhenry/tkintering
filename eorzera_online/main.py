ca3d49397d167f9628b6e09db9ea26c693a85bea
heyhenry <henryyy3@hotmail.com>
2024-08-05 23:58:52 +0800

    Initial commit for eorzera online

---
+++ eorzera_online/main.py
@@ -0,0 +1,13 @@
+# scenario: bossing checklist for multiple characters for a fictional game called Eorzera Online
+
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Eozera Online")
+root.geometry('300x300')
+
+main_screen = tk.Label(root, text='Characters Overview')
+main_screen.pack()
+
+root.mainloop()
