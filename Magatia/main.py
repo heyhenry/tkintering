b449d0582f04fc7368ad0112baa48619e1f78ab2
heyhenry <henryyy3@hotmail.com>
2024-09-14 12:25:46 +0800

    added content for rest of the sections

--- Magatia/main.py
+++ Magatia/main.py
@@ -34,13 +34,18 @@ rightbot_win.grid(row=2, column=1)
 title_lbl = ttk.Label(top_win, text='Magatia', font=(font_choice, 21))
 slogan_lbl = ttk.Label(top_win, text='~ Record your growing wisdom with us ~')
 
-title_lbl.grid(row=0, column=0)
-slogan_lbl.grid(row=1, column=0)
+title_lbl.pack(side='top')
+slogan_lbl.pack(side='bottom')
 
 # left_win
+entries_display = ttk.Label(left_win, text='Reading Entries', font=(font_choice, 14))
+
 entries_lb = tk.Listbox(left_win)
 entries_sb = ttk.Scrollbar(left_win, command=entries_lb.yview)
 entries_lb.config(yscrollcommand=entries_sb.set)
+
+entries_display.pack(side='top')
+
 entries_lb.pack(side='left', fill='both', expand=True)
 entries_sb.pack(side='right', fill='y')
 
@@ -52,5 +57,43 @@ remove_entry_btn = ttk.Button(leftbot_win, text='Delete Entry')
 add_entry_btn.pack()
 update_entry_btn.pack()
 remove_entry_btn.pack()
+
+# right_win 
+statistics_lbl = ttk.Label(right_win, text='Reading Statistics', font=(font_choice, 14))
+
+entries_completed_lbl = ttk.Label(right_win, text='Entries Completed:')
+entries_reading_lbl = ttk.Label(right_win, text='Entries Reading:')
+entries_dropped_lbl = ttk.Label(right_win, text='Entries Dropped:')
+entries_planned_lbl = ttk.Label(right_win, text='Entries Planned To Read:')
+
+entries_completed_stat_lbl = ttk.Label(right_win, text='100')
+entries_reading_stat_lbl = ttk.Label(right_win, text='100')
+entries_dropped_stat_lbl = ttk.Label(right_win, text='100')
+entries_planned_stat_lbl = ttk.Label(right_win, text='100')
+
+statistics_lbl.grid(row=0, columnspan=2)
+
+entries_completed_lbl.grid(row=1, column=0)
+entries_reading_lbl.grid(row=2, column=0)
+entries_dropped_lbl.grid(row=3, column=0)
+entries_planned_lbl.grid(row=4, column=0)
+
+entries_completed_stat_lbl.grid(row=1, column=1)
+entries_reading_stat_lbl.grid(row=2, column=1)
+entries_dropped_stat_lbl.grid(row=3, column=1)
+entries_planned_stat_lbl.grid(row=4, column=1)
+
+# rightbot_win
+badges_lbl = ttk.Label(rightbot_win, text='Badges', font=(font_choice, 14))
+
+lv_badge = ttk.Label(rightbot_win, text='Placeholder')
+chapter_badge = ttk.Label(rightbot_win, text='Placeholder')
+time_badge = ttk.Label(rightbot_win, text='Placeholder')
+
+badges_lbl.pack(side='top')
+
+lv_badge.pack(side='left')
+chapter_badge.pack(side='left')
+time_badge.pack(side='left')
 
 root.mainloop()
--- Magatia/resources_used.md
+++ Magatia/resources_used.md
@@ -1,5 +1,6 @@
 # A list of the resources used to help me achieve the completion of this project
 
-Creating a Notebook: https://www.tutorialspoint.com/notebook-widget-in-tkinter
+Creating a notebook: https://www.tutorialspoint.com/notebook-widget-in-tkinter
 Updating fonts globally: https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/ttk-style-layer.html
 Understanding pack() method: https://www.pythontutorial.net/tkinter/tkinter-pack/
+Implementing scrollbar: https://stackoverflow.com/questions/15795137/how-to-insert-a-scrollbar-and-a-form-in-a-notebook-widget-with-python-and-tkinte
