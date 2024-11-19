c152cad9eafdc3d30044840c7666c13a2f318114
heyhenry <henryyy3@hotmail.com>
2024-11-20 00:07:14 +0800

    implemented simple skeleton of login and homepage

--- track_workouts/main.py
+++ track_workouts/main.py
@@ -12,4 +12,45 @@ class MainApp(tk.Tk):
 
         self.pages = {}
 
-        
+        for P in (LoginPage, HomePage):
+            page = P(container, self)
+            self.pages[P] = page
+            page.grid(row=0, column=0, sticky='nswe')
+
+        self.show_page(LoginPage)
+
+    def show_page(self, cont):
+        page = self.pages[cont]
+        page.tkraise()
+
+class LoginPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.controller = controller
+        self.create_widgets()
+
+    def create_widgets(self):
+        hello = tk.Label(self, text='Hello, This is the Login Page!')
+        home_redirect = tk.Button(self, text='Home Page', command=lambda:self.controller.show_page(HomePage))
+
+        hello.pack()
+        home_redirect.pack()
+
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.controller = controller
+        self.create_widgets()
+
+    def create_widgets(self):
+        hello = tk.Label(self, text='Hello, This is the Home Page.')
+        login_redirect = tk.Button(self, text='Login Page', command=lambda: self.controller.show_page(LoginPage))
+
+        hello.pack()
+        login_redirect.pack()
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.mainloop()
