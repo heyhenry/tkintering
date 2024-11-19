edd7d986122b471e268922a717aa58956c037c2c
heyhenry <henryyy3@hotmail.com>
2024-11-19 23:59:31 +0800

    initial commit

---
+++ track_workouts/main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+class MainApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(side='top', fill='both', expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.pages = {}
+
+        
