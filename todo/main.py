29c710d63e9ad65b1a81759675c846ad72506768
heyhenry <henryyy3@hotmail.com>
2024-12-05 21:04:18 +0800

    added task enter prompt

--- todo/main.py
+++ todo/main.py
@@ -4,10 +4,18 @@ import tkinter as tk
 root = tk.Tk()
 root.title('To Do App.')
 root.geometry('800x600')
+
+task_var = tk.StringVar()
+
 # task input
+task_enter_title = tk.Label(root, text='Enter Task:', font=(24))
+task_enter_title.place(y=50, x=300)
+
+task_enter_prompt = tk.Entry(root, textvariable=task_var, font=(24))
+task_enter_prompt.place(y=50, x=400, width=300)
 # add task button
 
-# list added tasks
+# listbox for added tasks
 tasks_listbox_title = tk.Label(root, text='To Do Tasks:', font=(24))
 tasks_listbox_title.place(y=50, x=80)
 
@@ -17,6 +25,7 @@ tasks_listbox.place(y=80, x=30, width=200, height=300)
 tasks_listbox_scrollbar = tk.Scrollbar(root)
 tasks_listbox_scrollbar.place(y=80, x=230, height=300)
 
+# linking the listbox and scrollbar with eachother
 tasks_listbox.config(yscrollcommand=tasks_listbox_scrollbar.set)
 tasks_listbox_scrollbar.config(command=tasks_listbox.yview)
 
