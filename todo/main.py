281426565f964014e39a7c417519fd6a3c65b8d9
heyhenry <henryyy3@hotmail.com>
2024-12-05 20:57:45 +0800

    added listbox

--- todo/main.py
+++ todo/main.py
@@ -1,10 +1,20 @@
 # a simple todo app
+import tkinter as tk
 
-
+root = tk.Tk()
+root.title('To Do App.')
+root.geometry('800x600')
 # task input
 # add task button
 
 # list added tasks
+tasks_listbox_title = tk.Label(root, text='To Do Tasks:', font=(24))
+tasks_listbox_title.place(y=50, x=80)
+
+tasks_listbox = tk.Listbox(root)
+tasks_listbox.place(y=80, x=30, width=200, height=300)
+
+tasks_listbox_scrollbar = tk.Scrollbar(root)
 
 # remove task
 
@@ -13,3 +23,4 @@
 
 # clear all button
 
+root.mainloop()
--- todo/reference.md
+++ todo/reference.md
@@ -1,1 +1,2 @@
 # References Used:
+Scrollbar for listbox: https://www.geeksforgeeks.org/scrollable-listbox-in-python-tkinter/
