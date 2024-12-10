10c58992a83c3a9824ef0b64f45afe46e92ff672
heyhenry <henryyy3@hotmail.com>
2024-12-10 11:04:23 +0800

    implemented widget binds, get_equation and display_equation functions

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,23 +1,22 @@
 import tkinter as tk
 
-result = 0
+result = ''
+equation = []
 
 root = tk.Tk()
 
-# add number
-def add():
-    global result
-    result += float(user_input_var.get())
-    user_input_var.set(result)
+# get the user input values to create the equation
+def get_equation(mouse_event, val):
+    global equation
+    equation.append(val)
 
-def subtract():
+# show the equation and its result
+def display_equation():
+    print(equation)
     global result
-    result -= float(user_input_var.get())
-    user_input_var.set(result)
-
-# update the user_input_var variable's num value based on button pressed
-def update_user_input_var_value(mouse_event, num):
-    user_input_var.set(num)
+    for i in equation:
+        result += i
+    print(eval(result))
 
 user_input_var = tk.StringVar()
 
@@ -30,7 +29,7 @@ number_seven = tk.Button(root, text='7', **button_params)
 number_eight = tk.Button(root, text='8', **button_params)
 number_nine = tk.Button(root, text='9', **button_params)
 division_operator = tk.Button(root, text='/', **button_params)
-addition_operator= tk.Button(root, text='+', **button_params, command=add)
+addition_operator= tk.Button(root, text='+', **button_params)
 number_seven.grid(row=1, column=0)
 number_eight.grid(row=1, column=1)
 number_nine.grid(row=1, column=2)
@@ -51,7 +50,7 @@ empty_one.grid(row=2, column=4)
 number_one = tk.Button(root, text='1', **button_params)
 number_two = tk.Button(root, text='2', **button_params)
 number_three = tk.Button(root, text='3', **button_params)
-subtract_operator = tk.Button(root, text='-', **button_params, command=subtract)
+subtract_operator = tk.Button(root, text='-', **button_params)
 empty_two = tk.Button(root, text='', **button_params, state='disabled')
 number_one.grid(row=3, column=0)
 number_two.grid(row=3, column=1)
@@ -61,7 +60,7 @@ empty_two.grid(row=3, column=4)
 
 decimal_point = tk.Button(root, text='.', **button_params)
 number_zero = tk.Button(root, text='0', **button_params)
-equals_sign = tk.Button(root, text='=', **button_params)
+equals_sign = tk.Button(root, text='=', **button_params, command=display_equation)
 clear_sign = tk.Button(root, text='C', **button_params)
 all_clear_sign = tk.Button(root, text='AC', **button_params)
 decimal_point.grid(row=4, column=0)
@@ -70,17 +69,20 @@ equals_sign.grid(row=4, column=2)
 clear_sign.grid(row=4, column=3)
 all_clear_sign.grid(row=4, column=4)
 
-number_seven.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 7))
-number_eight.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 8))
-number_nine.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 9))
-number_four.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 4))
-number_five.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 5))
-number_six.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 6))
-number_one.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 1))
-number_two.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 2))
-number_three.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 3))
-number_zero.bind("<Button-1>", lambda mouse_event: update_user_input_var_value(mouse_event, 0))
+number_seven.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '7'))
+number_eight.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '8'))
+number_nine.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '9'))
+number_four.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '4'))
+number_five.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '5'))
+number_six.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '6'))
+number_one.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '1'))
+number_two.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '2'))
+number_three.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '3'))
+number_zero.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '0'))
 
-# addition_operator.bind("<Button-1>", )
+division_operator.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '/'))
+addition_operator.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '+'))
+multiplier_operator.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '*'))
+subtract_operator.bind("<Button-1>", lambda mouse_event: get_equation(mouse_event, '-'))
 
 root.mainloop()
